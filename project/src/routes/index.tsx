70f5b8574e9f966e62db626fa3457181ae18e303
lucas-fer-fig <figueiredo.lucasfernandes@gmail.com>
2020-11-05 16:27:06 -0400

    Upload website files

--- project/src/routes/index.tsx
+++ project/src/routes/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Dashboard from '../pages/Dashboard';
-import Repository from '../pages/Repository';
+import Dashboard from "../pages/Dashboard";
+import Repository from "../pages/Repository";
 
 // import { Container } from './styles';
 
 const Routes: React.FC = () => (
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Switch>
       <Route exact path="/" component={Dashboard} />
       <Route path="/repositories/:repository+" component={Repository} />
